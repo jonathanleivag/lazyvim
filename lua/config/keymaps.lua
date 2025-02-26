85f4b899b85420b253be21b1c2bfa8e4e3d08b65
Jonathanleivag <email@jonathanleivag.cl>
2025-02-26 18:06:21 -0300

    Config
    Config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,5 +16,3 @@ end, { desc = "Abrir en app predeterminada" })
 vim.keymap.set("n", "<leader>k", ":m-2<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>j", ":m+1<CR>", { noremap = true, silent = true })
 
-
-vim.api.nvim_set_keymap('n', '<leader>vm', ':ViMongo<CR>', { noremap = true, silent = true })
